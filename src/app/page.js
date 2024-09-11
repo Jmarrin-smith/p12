61a1809107aa2458014e89c020b6ae4f02962d72
Jmarrin-smith <marrinsmithj@gmail.com>
2024-09-11 14:15:00 +0100

    nav componant

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Link from "react-router-dom";
 
 // Define the HomePage component
 const HomePage = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/addlist">Add List</Link>
-          </li>
-          <li>
-            <Link to="/Search">Search</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-      </nav>
       <h1>Bracketiser</h1>
       <section>
         <h2>About</h2>
---
+++ src/app/pagecomponants/nav.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import styles from "./nav.module.css";
+
+export default function Nav() {
+  return (
+    <nav className={styles.container}>
+      <div className={styles.navitemscontainer}>
+        <div className={styles.placeholder}></div>
+        <div className={styles.home}>
+          <Link href="/">
+            <h1>Bracketiser</h1>
+          </Link>
+        </div>
+        <div className={styles.links}>
+          <Link href="/list">
+            <h3>list</h3>
+          </Link>
+          <Link href="/search">
+            <h3>search</h3>
+          </Link>
+          <Link href="/addlist">
+            <h3>add list</h3>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
