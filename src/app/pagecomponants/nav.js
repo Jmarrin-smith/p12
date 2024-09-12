f000e28233c3104f49645eadcd5d826bd33365d2
Christian Richard Fairhall Smith <fairhallsmith@gmail.com>
2024-09-12 09:37:47 +0100

    Adding Clerk components.

--- src/app/pagecomponants/nav.js
+++ src/app/pagecomponants/nav.js
@@ -1,28 +1,43 @@
 import Link from "next/link";
 import styles from "./nav.module.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 export default function Nav() {
   return (
-    <nav className={styles.container}>
-      <div className={styles.navitemscontainer}>
-        <div className={styles.placeholder}></div>
-        <div className={styles.home}>
-          <Link href="/">
-            <h1>Bracketiser</h1>
-          </Link>
+    <ClerkProvider>
+      <nav className={styles.container}>
+        <div className={styles.navitemscontainer}>
+          <div className={styles.placeholder}></div>
+          <div className={styles.home}>
+            <Link href="/">
+              <h1>Bracketiser</h1>
+            </Link>
+          </div>
+          <div className={styles.links}>
+            <Link href="/list">
+              <h3>list</h3>
+            </Link>
+            <Link href="/search">
+              <h3>search</h3>
+            </Link>
+            <Link href="/addlist">
+              <h3>add list</h3>
+            </Link>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
         </div>
-        <div className={styles.links}>
-          <Link href="/list">
-            <h3>list</h3>
-          </Link>
-          <Link href="/search">
-            <h3>search</h3>
-          </Link>
-          <Link href="/addlist">
-            <h3>add list</h3>
-          </Link>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </ClerkProvider>
   );
 }
