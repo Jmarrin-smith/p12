94dd4085df4506f29c022a4d8a2ca21a29299a3c
Christian Richard Fairhall Smith <fairhallsmith@gmail.com>
2024-09-18 16:32:28 +0100

    Fixing profile picture.

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+        </head>
         <body>
           <Nav />
           <SignedOut></SignedOut>
